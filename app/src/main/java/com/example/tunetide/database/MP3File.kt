fb0ec80c4668b7079b88856c5620f7a92bc8a278
kianamoazzami <moazzamikiana@gmail.com>
2024-06-12 13:52:58 -0400

    setup MP3 database

--- app/src/main/java/com/example/tunetide/database/MP3File.kt
+++ app/src/main/java/com/example/tunetide/database/MP3File.kt
@@ -1,5 +1,10 @@
 package com.example.tunetide.database
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-class MP3File {
-
-}
+@Entity(tableName = "MP3Files")
+data class MP3File (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val fileName: String,
+    val filePath: String
+)
