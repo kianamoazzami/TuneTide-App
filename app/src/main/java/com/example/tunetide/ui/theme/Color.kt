bbc76ebf1120c3c6ceefaa2a1fa43ee279f64d63
Nour Tayem <ntayem@uoguelph.ca>
2024-06-12 12:15:43 -0400

    color code of the main screen

--- app/src/main/java/com/example/tunetide/ui/theme/Color.kt
+++ app/src/main/java/com/example/tunetide/ui/theme/Color.kt
@@ -4,3 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 
 val PurpleMain = Color(0xC0BFE0)
+val titleColorCode = Color(0x544FA3)
+val textColorCode = Color(0x241673)
+val flowPreview = Color(0xBC67BA)
