d494a0b63610c9548e17ee98b8150bf1c6889050
Erica Baldesarra <ebaldesa@uwaterloo.ca>
2024-06-16 12:16:38 -0400

    Add framework for SpotifySDK as submodule

--- app/src/main/java/com/example/tunetide/MainActivity.kt
+++ app/src/main/java/com/example/tunetide/MainActivity.kt
@@ -12,9 +12,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tunetide.ui.theme.TuneTideTheme
 
+// Spotify SDK imports
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+import android.util.Log
+
+// Spotify Credentials.. define somewhere else once I understand kotlin and app dev lol
+// maybe should be in onCreate?
+
+private val clientId = "TuneTide" // ERICA TODO after spotify application
+private val redirectUri = "http://localhost:8080"
+private var spotifyAppRemote: SpotifyAppRemote? = null
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // ERICA TODO figure out the below line, ask UI
+        // setContentView(R.layout.activity_main)
         setContent {
             TuneTideTheme {
                 // A surface container using the 'background' color from the theme
@@ -27,5 +45,39 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Set the Spotify connection parameters
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                // Now you can start interacting with App Remote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        })
+    }
+
+    private fun connected() {
+        // Then we will write some more code here.
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // Aaand we will finish off here.
     }
 }
