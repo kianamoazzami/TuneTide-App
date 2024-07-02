555d051208e72ce881a9963aa823feb04a7817d0
Erica Baldesarra <ebaldesa@uwaterloo.ca>
2024-07-02 16:06:07 -0400

    Spotify Controller fixed

--- app/src/main/java/com/example/tunetide/spotify/SpotifyController.kt
+++ app/src/main/java/com/example/tunetide/spotify/SpotifyController.kt
@@ -8,25 +8,33 @@ import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
+import com.spotify.sdk.android.auth.AccountsQueryParameters
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
+import com.spotify.sdk.android.auth.LoginActivity
 
 private const val clientId = "cb2af3cb9add453d8a18f97e2aae117f"
-private const val redirectUri = "http://localhost/"
+private const val redirectUri = "tunetide://test"
 private var spotifyAppRemote: SpotifyAppRemote? = null
 
 
-class SpotifyController {
-    fun connect(activity: Context) {
+class SpotifyController(val mainContext: Context) {
+
+    private fun connectAndExecute(operation: () -> Any?): Any? {
         val connectionParams = ConnectionParams.Builder(clientId)
             .setRedirectUri(redirectUri)
             .showAuthView(true)
             .build()
 
-        SpotifyAppRemote.connect(activity, connectionParams, object : Connector.ConnectionListener {
+        var returnVal: Any? = null
+
+        SpotifyAppRemote.connect(mainContext, connectionParams, object : Connector.ConnectionListener {
             override fun onConnected(appRemote: SpotifyAppRemote) {
                 spotifyAppRemote = appRemote
+                // TODO: see if you can make appRemote persist.. otherwise have to do this wrapper way yuck
                 Log.d("MainActivity", "Connected! Yay!")
-                // Now you can start interacting with App Remote
-                return
+                returnVal = operation()
             }
 
             override fun onFailure(throwable: Throwable) {
@@ -34,6 +42,37 @@ class SpotifyController {
                 // Something went wrong when attempting to connect! Handle errors here
             }
         })
+
+        return returnVal;
+
+    }
+
+    // overload connect to take function with parameters
+    // TODO: this is clunky. I can't find a way to have kotlin take a function that may or may not have parameters
+    private fun connectAndExecute(operation: (Any?) -> Unit, params: Any? = null): Any? {
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+        var returnVal: Any? = null
+
+        SpotifyAppRemote.connect(mainContext, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                if (params != null) {
+                    returnVal = operation(params)
+                }
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        })
+
+        return returnVal;
     }
 
     fun disconnect() {
@@ -41,21 +80,21 @@ class SpotifyController {
             SpotifyAppRemote.disconnect(it)
         }
     }
-
     fun playSamplePlaylist() {
-        Log.d("MainActivity", "pressed play")
-        spotifyAppRemote?.let {
-            // Play a playlist
-            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
-            it.playerApi.play(playlistURI)
+        fun playSampleWrapper() {
             Log.d("MainActivity", "pressed play")
-            // Subscribe to PlayerState
-            it.playerApi.subscribeToPlayerState().setEventCallback {
-                val track: Track = it.track
-                Log.d("MainActivity", track.name + " by " + track.artist.name)
+            spotifyAppRemote?.let {
+                // Play a playlist
+                val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
+                it.playerApi.play(playlistURI)
+                Log.d("MainActivity", "PlayerAPI called")
+                // Subscribe to PlayerState
+                it.playerApi.subscribeToPlayerState().setEventCallback {
+                    val track: Track = it.track
+                    Log.d("MainActivity", track.name + " by " + track.artist.name)
+                }
             }
         }
-
+        connectAndExecute(::playSampleWrapper)
     }
-
 }
