d3d44db75ea5e9602916db61abd3634f4819b1eb
Nour Tayem <ntayem@uoguelph.ca>
2024-07-10 12:49:30 -0400

    Updating setting page

--- app/src/main/java/com/example/tunetide/ui/Settings.kt
+++ app/src/main/java/com/example/tunetide/ui/Settings.kt
@@ -15,10 +15,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tunetide.R
 import com.example.tunetide.ui.theme.PurpleBackground
+import com.example.tunetide.ui.theme.PurpleAccent
 
 class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,115 +34,131 @@ class SettingsActivity : ComponentActivity() {
 }
 
 @Composable
+fun TuneTideTopAppBarBack(navigateBack: () -> Unit) {
+    TopAppBar(
+        backgroundColor = PurpleBackground,
+        contentPadding = PaddingValues(horizontal = 1.dp)
+    ) {
+        IconButton(onClick = navigateBack) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_back_arrow),
+                contentDescription = "Back",
+                tint = PurpleAccent,
+                modifier = Modifier.size(20.dp)
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = "tunetide",
+            color = Color(0xFF544FA3),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier.weight(6f)
+        )
+        Spacer(modifier = Modifier.weight(2.3f))
+    }
+}
+
+@Composable
 fun SettingsScreen(onBackClick: () -> Unit) {
     var isDarkModeEnabled by remember { mutableStateOf(false) }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(PurpleBackground)
-            .padding(16.dp)
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_back_arrow),  // Ensure this matches your drawable
-                contentDescription = stringResource(id = R.string.back),
-                tint = Color(0xFF2B217F),  // Ensure the color matches your design
+    Scaffold(
+        topBar = {
+            TuneTideTopAppBarBack(navigateBack = onBackClick)
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(PurpleBackground)
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
                 modifier = Modifier
-                    .clickable { onBackClick() }
-                    .padding(8.dp)  // Add some padding around the icon
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Text(
-                text = "tunetide",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Normal,
-                color = Color(0xFF2B217F),
-                modifier = Modifier.weight(1f)  // Take up the remaining space
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .clickable { isDarkModeEnabled = !isDarkModeEnabled },  // Toggle dark mode
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = stringResource(id = R.string.dark_mode),
-                fontSize = 18.sp,
-                color = Color(0xFF333333),
-                modifier = Modifier.weight(1f)
-            )
-            Switch(
-                checked = isDarkModeEnabled,
-                onCheckedChange = { isDarkModeEnabled = it }
-            )
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Divider()
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .clickable { /* TODO: Implement Connect Spotify */ },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = stringResource(id = R.string.connect_spotify),
-                fontSize = 18.sp,
-                color = Color(0xFF333333),
-                modifier = Modifier.weight(1f)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.spotify_logo),  // Ensure this matches your drawable file name
-                contentDescription = stringResource(id = R.string.spotify),
-                modifier = Modifier.size(30.dp)  // Adjust the size as needed
-            )
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Divider()
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .clickable { /* TODO: Implement Manage Local Music Files */ },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = stringResource(id = R.string.manage_local_music_files),
-                fontSize = 18.sp,
-                color = Color(0xFF333333),
-                modifier = Modifier.weight(1f)
-            )
-            Icon(
-                painter = painterResource(id = R.drawable.ic_arrow_right),
-                contentDescription = null
-            )
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Divider()
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .clickable { /* TODO: Implement Schedule Settings */ },
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = stringResource(id = R.string.schedule_settings),
-                fontSize = 18.sp,
-                color = Color(0xFF333333),
-                modifier = Modifier.weight(1f)
-            )
-            Icon(
-                painter = painterResource(id = R.drawable.ic_arrow_right),
-                contentDescription = null
-            )
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable { isDarkModeEnabled = !isDarkModeEnabled },  // Toggle dark mode
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.dark_mode),
+                    fontSize = 18.sp,
+                    color = Color(0xFF333333),
+                    modifier = Modifier.weight(1f)
+                )
+                Switch(
+                    checked = isDarkModeEnabled,
+                    onCheckedChange = { isDarkModeEnabled = it }
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Divider()
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable { /* TODO: Implement Connect Spotify */ },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.connect_spotify),
+                    fontSize = 18.sp,
+                    color = Color(0xFF333333),
+                    modifier = Modifier.weight(1f)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.spotify_logo),  // Ensure this matches your drawable file name
+                    contentDescription = stringResource(id = R.string.spotify),
+                    modifier = Modifier.size(30.dp)  // Adjust the size as needed
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Divider()
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable { /* TODO: Implement Manage Local Music Files */ },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.manage_local_music_files),
+                    fontSize = 18.sp,
+                    color = Color(0xFF333333),
+                    modifier = Modifier.weight(1f)
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_right),
+                    contentDescription = null
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Divider()
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clickable { /* TODO: Implement Schedule Settings */ },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.schedule_settings),
+                    fontSize = 18.sp,
+                    color = Color(0xFF333333),
+                    modifier = Modifier.weight(1f)
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_right),
+                    contentDescription = null
+                )
+            }
         }
     }
 }
