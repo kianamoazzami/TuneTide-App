09e5c31d02faad883f55d37d1820916b3414b724
kianamoazzami <moazzamikiana@gmail.com>
2024-07-15 01:27:04 -0400

    changing back destination

--- app/src/main/java/com/example/tunetide/ui/navigation/TuneTideNavGraph.kt
+++ app/src/main/java/com/example/tunetide/ui/navigation/TuneTideNavGraph.kt
@@ -36,7 +36,7 @@ fun TuneTideNavHost(
 ) {
     NavHost(
         navController = navController,
-        startDestination = SettingsDestination.route, //TODO: change back to HomeDestination
+        startDestination = HomeDestination.route,
         modifier = modifier
     ) {
         composable(route = HomeDestination.route) {
