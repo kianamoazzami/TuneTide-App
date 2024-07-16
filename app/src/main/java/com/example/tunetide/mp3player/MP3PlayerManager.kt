33e295b2f760b2f156f1b278e79cda5f15e9279c
kianamoazzami <moazzamikiana@gmail.com>
2024-07-16 18:45:51 -0400

    playing mp3 playlists with timer

--- app/src/main/java/com/example/tunetide/mp3player/MP3PlayerManager.kt
+++ app/src/main/java/com/example/tunetide/mp3player/MP3PlayerManager.kt
@@ -3,47 +3,96 @@ package com.example.tunetide.mp3player
 import android.content.Context
 import android.media.MediaPlayer
 import android.net.Uri
-import androidx.compose.runtime.*
-import com.example.tunetide.R
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.example.tunetide.database.MP3File
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
 
-class MP3PlayerManager(var context: Context) {
-    private var mediaPlayer: MediaPlayer
-    private var currentTrack = R.raw.flowmusic // resID
-    var isPlaying by mutableStateOf(false)
-        private set
+class MP3PlayerManager(private val context: Context) {
+    private var mediaPlayer: MediaPlayer = MediaPlayer()
+    private var playlist: List<MP3File> = emptyList()
+    private var currentSongIndex: Int = 0
+    private var isPlaying by mutableStateOf(false)
+    private var playlistJob: Job? = null
 
     init {
-        mediaPlayer = MediaPlayer()
         mediaPlayer.setOnPreparedListener {
             if (isPlaying) {
                 mediaPlayer.start()
             }
         }
-        switchTrack(currentTrack)
-    }
-
-    fun togglePlayPause() {
-        if (isPlaying) {
-            mediaPlayer.pause()
-        } else {
-            mediaPlayer.start()
+        mediaPlayer.setOnCompletionListener {
+            skipToNextSong()
         }
-        isPlaying = !isPlaying
+
+        mediaPlayer.setOnErrorListener { mp, what, extra ->
+            true  // Return true to indicate you've handled the error
+        }
     }
 
-    fun switchTrack(trackResId: Int) {
-        currentTrack = trackResId
-
-        mediaPlayer.reset()
-        mediaPlayer.setDataSource(context, getUri(currentTrack))
-        mediaPlayer.prepareAsync()
+    fun play() {
+        if (playlist.isEmpty()) {
+            return
+        }
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+            isPlaying = true
+        }
     }
 
-    fun onDestroy() {
+    fun pause() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.pause()
+            isPlaying = false
+        }
+    }
+
+    private fun playCurrentSong() {
+        if (playlist.isNotEmpty() && currentSongIndex < playlist.size) {
+            val currentSong = playlist[currentSongIndex].filePath
+            mediaPlayer.reset()
+
+            val uri = Uri.parse(currentSong)
+            mediaPlayer.setDataSource(context, uri)
+
+            mediaPlayer.prepareAsync()
+        }
+    }
+
+    fun skipToNextSong() {
+        currentSongIndex++
+        if (currentSongIndex >= playlist.size) {
+            stopMusic()
+        } else {
+            playCurrentSong()
+        }
+    }
+
+    fun stopMusic() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+        }
+        isPlaying = false
+    }
+
+    fun switchPlaylist(newPlaylistFlow: Flow<List<MP3File>>) {
+        playlistJob?.cancel()
+        playlistJob = CoroutineScope(Dispatchers.IO).launch {
+            newPlaylistFlow.collectLatest { newPlaylist ->
+                withContext(Dispatchers.Main) {
+                    playlist = newPlaylist
+                    currentSongIndex = 0
+                    playCurrentSong()
+                }
+            }
+        }
+    }
+
+    fun releaseMediaPlayer() {
         mediaPlayer.release()
-    }
-
-    private fun getUri(resId: Int): Uri {
-        return Uri.parse("android.resource://${context.packageName}/$resId")
     }
 }
