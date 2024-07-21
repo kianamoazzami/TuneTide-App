f04866eef333e7741aaa8e0d77015ced0a9f3db5
ebald19 <ebaldesarra19@gmail.com>
2024-07-21 11:28:18 -0400

    Remove testing code

--- app/src/main/java/com/example/tunetide/Application.kt
+++ app/src/main/java/com/example/tunetide/Application.kt
@@ -3,9 +3,6 @@ package com.example.tunetide
 import android.app.Application
 import android.util.Log
 import com.example.tunetide.spotify.SpotifyController
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class Application : Application() {
 
@@ -19,12 +16,6 @@ class Application : Application() {
         Log.d("TuneTideApplication", "Initializing container")
         container = AppDataContainer(this)
         mainSpotifyController.loadPlaylists(container.spotifyRepository)
-
-        mainSpotifyController.playUri("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL")
-
-        CoroutineScope(Dispatchers.Main).launch {
-            Log.d("ERICA TEST", mainSpotifyController.getCurrentTrackName())
-        }
 
 
 
