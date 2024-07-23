9107e2f2d95c948cb9c38270d53197fe503e5433
kianamoazzami <moazzamikiana@gmail.com>
2024-07-23 12:47:28 -0400

    colour changes for accessibility on timer page

--- app/src/main/java/com/example/tunetide/ui/timers/BoxWithImage.kt
+++ app/src/main/java/com/example/tunetide/ui/timers/BoxWithImage.kt
@@ -3,23 +3,31 @@ package com.example.tunetide.ui.timers
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.tunetide.R
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import com.example.tunetide.ui.theme.PurpleAccent
 import com.example.tunetide.ui.theme.PurpleDark
-import com.example.tunetide.ui.theme.PurpleLight
 import com.example.tunetide.ui.theme.PurpleLight2
 
 data class BoxWithImageItem(
